6457312d7bdbe5ed68d6a2fe678c35795363b10c
Dheepak Krishnamurthy <me@kdheepak.com>
2020-07-21 07:14:24 -0600

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -14,6 +14,9 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd mdbtools/
+if [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]; then
+    atomic_patch -p1 "${WORKSPACE}/srcdir/patches/locale_header.patch"
+fi
 autoreconf -if
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-man
 make -j${nproc}
